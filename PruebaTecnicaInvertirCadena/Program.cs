705c224a30422ebbb01fb5605bdd6849c8768527
Fran Diaz <frandiazguitar@gmail.com>
2023-08-31 17:35:01 +0200

    Agregar archivos de proyecto.

---
+++ PruebaTecnicaInvertirCadena/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaInvertirCadena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadenaTexto = "Hola, soy una cadena de texto";
+
+            string cadenaInvertida = "";
+
+            //Forma 1
+            for (int i = (cadenaTexto.Length-1); i >= 0; i--)
+            {                
+                cadenaInvertida += Convert.ToString(cadenaTexto[i]);                
+            }
+
+            Console.WriteLine(cadenaInvertida);
+
+            //Forma 2
+            char[] chars = cadenaInvertida.ToCharArray();
+            Array.Reverse(chars);
+            Console.WriteLine(chars);
+        }
+    }
+}
